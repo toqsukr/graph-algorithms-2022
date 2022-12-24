389080abba2573345dd52734aa60b41a3a7b635e
temamal2003 <105546080+temamal2003@users.noreply.github.com>
2022-12-25 02:22:04 +0300

    test_added

--- src/test/java/org/example/GraphTestTest.java
+++ src/test/java/org/example/GraphTestTest.java
@@ -12,48 +12,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GraphTestTest {
 
     @Test
-    void MyTest1() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/dv.txt"));
+    void TwoLinked() throws FileNotFoundException {
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/two_linked.txt"));
 
         GraphDvusvaz GraphTest = new GraphDvusvaz();
         assertThat(GraphTest.execute(testGraph)).isEqualTo(true);
     }
 
     @Test
-    void MyTest2() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/dvaugol.txt"));
+    void TwoAngle() throws FileNotFoundException {
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph_two_angle.txt"));
 
         GraphDvusvaz GraphTest = new GraphDvusvaz();
         assertThat(GraphTest.execute(testGraph)).isEqualTo(false);
     }
 
     @Test
-    void MyTest3() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/line.txt"));
+    void TwoVertices() throws FileNotFoundException {
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph_on_two_vertices.txt"));
 
         GraphDvusvaz GraphTest = new GraphDvusvaz();
         assertThat(GraphTest.execute(testGraph)).isEqualTo(false);
     }
 
     @Test
-    void MyTest4() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/ndv.txt"));
+    void NotTwoLinked() throws FileNotFoundException {
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/not_two_linked.txt"));
 
         GraphDvusvaz GraphTest = new GraphDvusvaz();
         assertThat(GraphTest.execute(testGraph)).isEqualTo(false);
     }
 
     @Test
-    void MyTest5() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/nesv.txt"));
+    void NotConnected() throws FileNotFoundException {
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/not_connected.txt"));
 
         GraphDvusvaz GraphTest = new GraphDvusvaz();
         assertThat(GraphTest.execute(testGraph)).isEqualTo(false);
     }
 
     @Test
-    void MyTest6() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/ugol.txt"));
+    void GraphAngles() throws FileNotFoundException {
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph_angle.txt"));
 
         GraphDvusvaz GraphTest = new GraphDvusvaz();
         assertThat(GraphTest.execute(testGraph)).isEqualTo(false);
